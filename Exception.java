52ca7dce2b0e0e0514433fba9feaab3d0e647d0e
Lenovo <akashkamble8599@gmail.com>
2024-11-17 21:45:17 +0530

    commited file

--- Exception.java
+++ Exception.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Exception {
+public class Exception {
+    public static void main(String[] args) {
+        try
+        {
+            String b = "hi";
+            String c = new String("hi");
+            System.out.println(b.equals(c));
+            System.out.println(b==c);
+            b.toUpperCase();
+            System.out.println(b);
+            b =b.toUpperCase();
+            System.out.println(b);
+            int a = 1/0;
+        }
+        catch (ArithmeticException e){
+            System.out.println("In arithmetic exception");
+        }
+        catch (java.lang.Exception e){
+            System.out.println("In exception");
+        }
+    }
 }
