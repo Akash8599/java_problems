b18845adb4a3dbc26fbf6e7004246026c70a6a0e
Lenovo <akashkamble8599@gmail.com>
2024-11-17 21:52:40 +0530

    updated file

--- Test.java
+++ Test.java
@@ -1,21 +1,22 @@
-public class Test {
-
-    public static void main(String[] args) {
-
-        try
-        {
-            int i , sum;
-            sum = -10;
-            for (i = -1; i< 3; ++i){
-                sum = (sum/i);
-                System.out.println(i);
-            }
-        }
-        catch (ArithmeticException e){
-            System.out.println("0");
-        }
-
-    }
-
+ class A {
+    int x = 20;
 }
+ class B extends A {
+    int x = 30;
+}
+public class Test {
+    public static void main(String[] args)
+    {
+        B b = new B();
+        System.out.println(b.x);   // 30
 
+        A a = new A();
+        System.out.println(a.x);  // 20
+
+        A a2 = new B();
+        System.out.println(a2.x);  //30
+//
+//        B b2 = new A();
+//        System.out.println(b2.x);  // error
+    }
+}
